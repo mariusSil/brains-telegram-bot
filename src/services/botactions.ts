9e4e5fdc43e44d91e2a504a906a9b668fe8188c9
MariusSilenskis <marius.silenskis@gmail.com>
2025-01-10 12:38:09 +0100

    buy

--- src/services/botactions.ts
+++ src/services/botactions.ts
@@ -251,8 +251,8 @@ export class BotActions {
               ],
               [
                 {
-                  text: '🌟 Buy on KyberSwap',
-                  url: 'https://kyberswap.com/swap/base/usdc-to-0xf25b7dd973e30dcf219fbed7bd336b9ab5a05dd9',
+                  name: '🌟 Buy on Uniswap',
+                  url: 'https://app.uniswap.org/swap?chain=base&inputCurrency=0x0b3e328455c4059eeb9e3f84b5543f74e24e7e1b&outputCurrency=0xf25b7dd973e30dcf219fbed7bd336b9ab5a05dd9&value=137&field=input',
                 },
               ],
               [
